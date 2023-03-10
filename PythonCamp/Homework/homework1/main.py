1d6168b76749c5de6e1e990882e5f9948713b8b6
Cahit Can ÖZGEN <75495961+cahitcanozgenn@users.noreply.github.com>
2023-03-10 13:53:20 -0800

    PythonCamp

---
+++ PythonCamp/Homework/homework1/main.py
@@ -0,0 +1,6 @@
+loginMi=False
+if(loginMi):
+    print("Kurslarım-Tüm Kurslar-Kariyer-Sık Sorulan Sorular-User Logosu")
+else:
+    print("Kariyer-Sık Sorulan Sorular-Giriş Yap Kayıt Ol")
+   
---
+++ PythonCamp/main.py
@@ -0,0 +1,42 @@
+print("Kodlama.io")
+baslik = "Taşıt Kredisi"
+print(baslik)
+# string => metinsel ifade
+baslik = "İhtiyaç Kredisi"
+print(baslik)
+#int,integer => tam sayı
+vade = 36 # nümerik ifade
+ekVade = 8
+vade2 = "36" # alfabetik
+#float,decimal,double => ondalıklı sayı
+aylikOdeme = 285.50
+
+#bool,boolean => True/False
+yukselisteMi = True #pythonda büyük T harfi ile kullanılır
+
+#matematiksel operatörler
+print(8 + 6)
+print(vade + 12)
+print("\n")
+print(vade + ekVade)
+
+print(1<2)
+
+
+# or and
+
+
+print(1>2 or 5>2)
+
+#karar yapılar 
+#if else
+sayi1 = 10
+sayi2 = 20
+#sayi1>sayi2 ekrana sayi1 daha büyük
+
+#indent
+if sayi1>sayi2:
+    print("En Büyük Sayı: ",sayi1)
+else:
+    print("En Büyük Sayı:",sayi2)
+
